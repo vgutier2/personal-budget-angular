2609d27b511744bd6177c9fec8c6ef9d77a060dc
Velina Gutierrez <velinagutierrez@Velinas-MBP.lan>
2020-10-07 23:37:38 -0400

    Added Cors

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 4000;
-
-app.use('/', express.static('public'));
-
-app.get('/hello', (req, res) => {
-    res.send('Hello World!');
-});
-
 const budget = require('./data.json');
+
+app.use(cors());
+
 app.get('/budget', (req, res) => {
     res.json(budget);
 });
 
 app.listen(port, () => {
-    console.log('Example app listening at http://localhost:${port}')
+    console.log('API served at http://localhost:${port}');
 });
